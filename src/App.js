a3fa22e8e4777695db7df8f26c0605aaf60f9330
PrincePatel22 <editingroom99@gmail.com>
2023-02-27 22:32:29 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import Dashboard from './components/index';
-import Footer from './components/footer';
+import "./App.css";
+import Dashboard from "./components/index";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="App">
-     <Dashboard />
-     <Footer />
-      </div>
-      );
+      <Dashboard />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
